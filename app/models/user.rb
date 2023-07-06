9ed5e6038008733cfbf07d580c9e8c9150b9f6a8
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-07 00:42:29 +0500

    Add Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates: email, presence :true, length: {maximum:105}, uniqueness: true
+    validates: username, presence :true, length: {minimum:3, maximum:25}, uniqueness: true
 end
