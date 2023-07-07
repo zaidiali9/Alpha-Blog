d4af57f7bc22ac00c26641f41624fc656376840d
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-08 01:42:43 +0500

    User Validation Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-    validates: email, presence :true, length: {maximum:105}, uniqueness: true
-    validates: username, presence :true, length: {minimum:3, maximum:25}, uniqueness: true
+    validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
