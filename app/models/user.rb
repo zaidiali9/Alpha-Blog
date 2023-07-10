1365f030031493a1105f6a8c314e522922790e14
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-10 12:46:58 +0500

    One to Many Association Has Added In Artciles to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    validates: email, presence :true, length: {maximum:105}, uniqueness: true
-    validates: username, presence :true, length: {minimum:3, maximum:25}, uniqueness: true
+    has_many :articles
+    validates :email, presence: true, length: {maximum:105}, uniqueness: { case_sensitive: false}
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, length: {minimum:3, maximum:25}, uniqueness: { case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+
 end
